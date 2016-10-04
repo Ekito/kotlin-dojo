2f20c3e8fb07e2b54333dadc6e1aad1c2a3c72be
Arnaud Giuliani <agiuliani@ekito.fr>
2016-10-04 16:10:42 +0200

    make file compile

--- string-calc/src/test/kotlin/FirstTest.kt
+++ string-calc/src/test/kotlin/FirstTest.kt
@@ -15,45 +15,45 @@ class FirstTest {
             if (input.isEmpty())
                 return 0
             else {
-                return input.split(",").flatMap { s -> s.split("\n") }.map { s -> Integer.parseInt(s) }.map {
-                    i ->  i < 0 ? -1 : 42
-                }.sum()
-        }
-
-
-        @Test
-        fun testThatEmptyStringReturnsZero() {
-            val calculator = StringCalculator()
-            Assert.assertEquals(0, calculator.sum(""))
-        }
-
-        @Test
-        fun testThatSingleNumberReturnsItsvalue() {
-            val calculator = StringCalculator()
-            Assert.assertEquals(42, calculator.sum("42"))
-        }
-
-        @Test
-        fun testThatTwoCommaSeparatedNumbersReturnsTheirSum() {
-            val calculator = StringCalculator()
-            Assert.assertEquals(84, calculator.sum("42,42"))
-        }
-
-        @Test
-        fun testThatTwoNewlineSeparatedNumbersReturnsTheirSum() {
-            val calculator = StringCalculator()
-            Assert.assertEquals(84, calculator.sum("42\n42"))
-        }
-
-        @Test
-        fun testThatThreeNumbersSeparatedEitherWayReturnsTheirSum() {
-            val calculator = StringCalculator()
-            Assert.assertEquals(126, calculator.sum("42,42\n42"))
-        }
-
-        @Test(expected = IllegalArgumentException::class)
-        fun testThatNegativeNumbersThrowException() {
-            val calculator = StringCalculator()
-            calculator.sum("-42")
+                return input.split(",").flatMap { s -> s.split("\n") }.map { s -> Integer.parseInt(s) }.sum()
+            }
         }
     }
+
+
+    @Test
+    fun testThatEmptyStringReturnsZero() {
+        val calculator = StringCalculator()
+        Assert.assertEquals(0, calculator.sum(""))
+    }
+
+    @Test
+    fun testThatSingleNumberReturnsItsvalue() {
+        val calculator = StringCalculator()
+        Assert.assertEquals(42, calculator.sum("42"))
+    }
+
+    @Test
+    fun testThatTwoCommaSeparatedNumbersReturnsTheirSum() {
+        val calculator = StringCalculator()
+        Assert.assertEquals(84, calculator.sum("42,42"))
+    }
+
+    @Test
+    fun testThatTwoNewlineSeparatedNumbersReturnsTheirSum() {
+        val calculator = StringCalculator()
+        Assert.assertEquals(84, calculator.sum("42\n42"))
+    }
+
+    @Test
+    fun testThatThreeNumbersSeparatedEitherWayReturnsTheirSum() {
+        val calculator = StringCalculator()
+        Assert.assertEquals(126, calculator.sum("42,42\n42"))
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testThatNegativeNumbersThrowException() {
+        val calculator = StringCalculator()
+        calculator.sum("-42")
+    }
+}
